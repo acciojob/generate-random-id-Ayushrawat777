c627d4459f9fa7373cb633179f2a0f27ab56231f
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-03-29 01:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function makeid(l) {
-  // write your code here
-}
+function makeid(length) {
+        const chars =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let result = "";
+        for (let i = 0; i < length; i++) {
+          result += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return result;
+      }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
